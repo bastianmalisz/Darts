750658d17accaf6679063a260bc5e29019d9aa64
sebamalisz <malisz654321@gmail.com>
2018-03-18 11:50:38 +0100

    zmiany w rzutki games

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,13 +2,27 @@ const playerAmountBtn  = document.querySelector(".playerAmount-btn");
 const playerAmountInput = document.querySelector(".playerAmount--input");
 let playerAmountValue = 0;
 let names = [];
+let allScores = [];
+let highe=[]
 let clickCount=0 ;
 let lineNumber = 0;
 let itemCount =0;
 let highestScore=0;
+let secondScore = 0;
+let thirdScore = 0;
 let next = 0;
 let whoScores="";
+let whoScores2 = "", whoScores3 = "";
 let firstPlace, secondPlace, thirdPlace;
+
+let date = new Date();
+let month = date.getUTCMonth() + 1; //miesiac
+let day = date.getUTCDate(); // dzien
+let year = date.getYear();
+let nowDate = day+"."+month+"."+(year-100);
+
+document.querySelector(".gameWindow--gameDay").innerHTML = nowDate;
+
 playerAmountBtn.addEventListener("click",function(){
     if(playerAmountInput.value <1 || playerAmountInput.value > 6){
         alert("Musisz wybrać liczbę graczy od 1 do 6");
@@ -53,7 +67,30 @@ function addPlayers(playerAmountValue){
 function drawMainPage(){
     document.querySelector(".players").classList.add("displayNone");
     document.querySelector(".gameWindow").classList.remove("displayNone");
+
+    // icze czas gry 
+    let seconds = 0;
+    let mins = 0;
+    let secsAfter;
+    let el = document.querySelector('.gameWindow--gameTime');
+
+function incrementSeconds() {
+    seconds += 1;
+    secsAfter = seconds;
     
+    if(seconds  / 60 == 1 || seconds > 60 ){
+        seconds = 0;
+        mins+=1;
+        el.innerText = mins+" minut i "+secsAfter+ " sekund.";
+    }else if(mins >= 1){
+        el.innerText = mins+" minut i "+seconds+ " sekund.";
+    }else
+        el.innerText = seconds + " sekund.";
+    }
+    let cancel = setInterval(incrementSeconds, 1000);
+
+
+
     // Tworzę strukturę tabeli wyników
     const tableRow = [50];
     for(let i = 0; i <50;i++){
@@ -67,9 +104,10 @@ function drawMainPage(){
     }
     
     document.querySelector(".gameWindow__tableBox__table").appendChild(tableRow[0]);
-    tableRow[0].classList.add("gameWindow__tableBox__table--tr");
+    tableRow[0].classList.add("gameWindow__tableBox__table--tr-1");
+    
     for(let i=0;i<playerAmountValue;i++){
-        document.querySelector(".gameWindow__tableBox__table--tr").appendChild(tableHeader[i])
+        document.querySelector(".gameWindow__tableBox__table--tr-1").appendChild(tableHeader[i])
         tableHeader[i].innerHTML=names[i];
     }
     lineNumber = newLine(lineNumber);
@@ -107,16 +145,31 @@ function drawMainPage(){
         let tableCellDiv = document.createElement("div");
         let throwScore = parseInt(document.querySelector(".gameWindow__tableBox__input").value);
         
-        // Rejestrowanie najwyzeszego trafienia i kto trafil
+      
+ 
+
+
+    
+        // Rejestrowanie najwyzszych trafien i kto trafil
         if(throwScore > highestScore){
             highestScore = throwScore;
             whoScores = names[clickCount-1];
         }
-        
-        // Aktualizacja wyniku w tabeli rekordow
-        document.querySelector(".gameWindow__facts__bestThrow--who").innerHTML=whoScores;
-        document.querySelector(".gameWindow__facts__bestThrow--amount").innerHTML=highestScore;
 
+        
+    
+        
+
+        
+
+
+        // Aktualizacja wyniku w tabeli rekordow
+        
+            
+            document.querySelector(".gameWindow__facts__bestThrow--who").innerHTML=whoScores;
+            document.querySelector(".gameWindow__facts__bestThrow--amount").innerHTML=highestScore;
+        
+      
         console.log("rekord wynosi: "+highestScore, "rekord nalezy do: "+whoScores,"Ostatni rzut: "+throwScore,)
         tableCell.classList.add("table__cell");
         document.getElementById("table--tr"+lineNumber+"").appendChild(tableCell);
@@ -135,10 +188,15 @@ function drawMainPage(){
             tableCell.innerHTML = throwScore;
         }
         
+        let playerResult = [];
+        
+        //Liczenie wyniku obecnego kazdego z graczy i przypisywanie im
+     
+
     
     
         
-        console.log(clickCount,playerAmountValue,lineNumber,itemCount,next)
+        console.log(clickCount,playerAmountValue,lineNumber,itemCount,next,playerResult)
         if(clickCount == (parseInt(playerAmountValue))){
             let cells = document.querySelectorAll(".table__cell");
             let lastChilds = [];
@@ -150,22 +208,25 @@ function drawMainPage(){
              for(let i =1; i<=playerAmountValue;i++){
                 lastChilds[i] = cells[cells.length - i];
                 lastRowResult[i-1] = parseInt(lastChilds[i].textContent);
-            
+                
 
-                if( lastRowResult[i-1] >  lastRowResult[i]){
-                    console.log("wiekszy jest "+lastRowResult[i-1])
-                }else{
-                    console.log("wiekszy jest "+lastRowResult[i+1])
-                }
-                console.log(lastRowResult);
-                if(lastRowResult[i] > 500){
+               
+
+                
+                // if( lastRowResult[i-1] >  lastRowResult[i]){
+                //     console.log("wiekszy jest "+lastRowResult[i-1])
+                // }else{
+                //     console.log("wiekszy jest "+lastRowResult[i+1])
+                // }
+                console.log(lastRowResult.length-1, playerResult);
+                if(lastRowResult[lastRowResult.length-1] > 500){
                     alert("KONIEC GRY");
                 }
 
                 }
             clickCount = 0;
             newLine(lineNumber);
-            console.log("NOWA LINIA")
+            console.log("NOWA LINIA", lastRowResult)
             lineNumber++;
             
         }
