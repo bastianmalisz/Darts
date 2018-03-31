7592be8af4a53bcc796e0d78f65df48825314a01
Sebastian Maliszewski <malisz654321@gmail.com>
2018-03-31 21:16:06 +0200

    maany changes

--- scripts/sendinfo.js
+++ scripts/sendinfo.js
@@ -1,5 +1,24 @@
-function sendInfo(){
-    
+function sendInfo(results) {
+
+    //Wysylam info: co - nazwa zmiennej
+    // imiona wszystkich graczy (w kolejnosci dodawania) - names[]
+    // wyniki wszystkich graczy (w kolejnosci) lastRowResult
+    // najlepszy rzut - highestscore
+    // imie osoby ktora rzucila najlepiej - whoScores
+
+
+
+    // Przetwarzanie wyników
+    results = [];
+    for (let i = 0; i < playerAmountValue; i++) {
+        results.push({
+            name: names[i],
+            score: lastRowResult[i]
+        })
+    }
+    console.log(whoScores, results);
+
     alert("Dziękuję, informacje zostały wysłane do bazy danych");
-    document.location.reload();
+    // document.location.reload();
+    return results;
 }
